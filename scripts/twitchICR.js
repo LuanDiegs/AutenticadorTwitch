6cacee966fafd2e263e2c9056c910e88ce5405b8
Luan Eto <luan.eto18@gmail.com>
2024-03-12 17:49:31 -0300

    Redireciona para um host dinâmico e ajustes no localstorage

--- scripts/twitchICR.js
+++ scripts/twitchICR.js
@@ -11,19 +11,19 @@ function salvaNomeCanal(){
     const inputNomeCanal = document.getElementById("canal").value;
 
     if(inputNomeCanal){
-        localStorage.setItem("canal", inputNomeCanal.toLowerCase());
+        setLocalStorage("canal", inputNomeCanal.toLowerCase(), 60);
     }
 }
 
 function logaNaTwitch(){
     const oAuth = pegarStringEntre2Caracteres("token=", "&scope", document.location.hash, 6);
-    const nomeCanal = localStorage.getItem("canal");
+    const nomeCanal = getLocalStorage("canal");
 
     if(oAuth && nomeCanal){
         const socket = new WebSocket('wss://irc-ws.chat.twitch.tv:443');
         const textoChat = document.getElementById("chat");
         const canalCerto = nomeCanal;
-        console.log(canalCerto);
+
         socket.addEventListener('open', (e) => {
             socket.send(`PASS oauth:${oAuth}`);
             socket.send(`NICK ${canalCerto}`);
--- scripts/urlTwitch.js
+++ scripts/urlTwitch.js
@@ -1,14 +1,15 @@
 var rand = function() {return Math.random().toString(36).substr(2);};
 var token = function() {return rand() + rand();};
+var host = window.location.host;
 
 const urlVincular = "https://id.twitch.tv/oauth2/authorize?"+
     "response_type=token&"+
     "client_id=r007sc1iyvfsezaybfa5lfcw9gw62k&"+
-    "redirect_uri=http://localhost:8080&"+
+    `redirect_uri=http://${host}&`+
     "scope=chat%3Aread&"+
     `state=${token()}`;
 
 document.getElementById("botaoVincular").href = urlVincular;
 
-const nomeCanalSalvo = localStorage.getItem("canal");
+const nomeCanalSalvo = getLocalStorage("canal");
 if(nomeCanalSalvo) document.getElementById("canal").value = nomeCanalSalvo;
