f461a021b6fdf6fd701acc428e399edac9b42985
Luan Eto <luan.eto18@gmail.com>
2024-03-12 17:58:51 -0300

    Url dinamico para redirecionamento

--- scripts/urlTwitch.js
+++ scripts/urlTwitch.js
@@ -1,11 +1,16 @@
 var rand = function() {return Math.random().toString(36).substr(2);};
 var token = function() {return rand() + rand();};
-var host = window.location.host;
+var host = window.location.origin;
+
+//Caso tiver pathname ele coloca na url
+if(window.location.pathname != "/"){
+    host = host + window.location.pathname;
+}
 
 const urlVincular = "https://id.twitch.tv/oauth2/authorize?"+
     "response_type=token&"+
     "client_id=r007sc1iyvfsezaybfa5lfcw9gw62k&"+
-    `redirect_uri=http://${host}&`+
+    `redirect_uri=${host}&`+
     "scope=chat%3Aread&"+
     `state=${token()}`;
 
